44f1cf5cbb6609121c7a86b84127b60bc2e659e7
vjnambi <vnambi3@gatech.edu>
2022-01-02 22:44:14 -0700

    Update missionStatement.js

--- components/missionStatement.js
+++ components/missionStatement.js
@@ -2,6 +2,6 @@ import styles from '../styles/Home.module.css'
 
 export default function missionStatement(){
     return (
-        <div className={styles.prettyBox}>100% Gaming; 0% Blunders; Martin Baseba</div>
+        <div className={styles.prettyBox}>100% Gaming; 0% Blunders</div>
     )
 }
