0cb39084f469660cee65e34acd47a1d7b8f9633e
vjnambi <vjnambi@gmail.com>
2022-09-07 16:37:15 -0400

    Changed key from read-write to read-only

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {};
 
 config.endpoint = "https://smewebsite.documents.azure.com:443/";
-config.key =  "Q0gP9e0Uj2K1NLK5OMUeRuWJIv65bfWzTWEGqXlh23GDb4aCAgeMnAZLKzRmHE1AccVyLXXSNEvwSWuqbG0t8Q==";
+config.key =  "WdPVeByrolXvc3VM7JLpeuylk3UMnYuArEdzT3U13IMoAPrQeGjQBLkyiJ2mbz2Ket3EEPwASxRNyq5G9bxpSA==";
 config.database = "smewebsite";
 config.membersContainer = "Members";
 
